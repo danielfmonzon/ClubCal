dbe70a02062cba75c333677f112861dc34175eca
Caleb Oxborough <oxboroughcaleb@gmail.com>
2024-03-23 11:57:25 -0400

    add class def

--- Calendar/calendar.py
+++ Calendar/calendar.py
@@ -1,1 +1,13 @@
+class Calendar:
+    def __init__(self):
+        self.weeks = [[None] * 5 for _ in range(4)]
 
+class Day:
+    def __init__(self, date):
+        self.date = date
+        self.event_labels = []
+        self.event_times = []
+
+    def add_event(self, event_label, event_time):
+        self.event_labels.append(event_label)
+        self.event_times.append(event_time)
