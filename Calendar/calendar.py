122edec2da0b1ffeb2eaf32dd2d65aa7ae2cb79b
Caleb Oxborough <oxboroughcaleb@gmail.com>
2024-03-23 12:47:17 -0400

    Revert "event adding functionality"
    This reverts commit 42c9332de64e1b7f4b785de2e07c4504ffdf3699.

--- Calendar/calendar.py
+++ Calendar/calendar.py
@@ -10,25 +10,16 @@ class Day:
         self.date = date
         self.events = []
 
-    def add_event(self, event_name, event_start_time, event_duration):
-        self.events.append(Event(event_name, event_start_time, event_duration))
+    def add_event(self, event):
+        self.events.append(event)
 
 
 class Calendar:
     def __init__(self):
-
-        day_map = {
-            0: "Monday",
-            1: "Tuesday",
-            2: "Wednesday",
-            3: "Thursday",
-            4: "Friday"
-        }
-
         self.weeks = [[], [], [], [], []]
         for i in range(4):
             for j in range(5):
-                self.weeks[i].append(Day(day_map[j], "day"))
+                self.weeks[i].append(Day(j, "day"))
 
 
 
