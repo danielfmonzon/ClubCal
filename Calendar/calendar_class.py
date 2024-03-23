12ff4fa1205433b34f6e78c330e7f975bb48760d
Caleb Oxborough <oxboroughcaleb@gmail.com>
2024-03-23 15:25:21 -0400

    adding new files for sheet reading

--- Calendar/calendar_class.py
+++ Calendar/calendar_class.py
@@ -19,11 +19,13 @@ class Calendar:
     def __init__(self):
 
         day_map = {
-            0: "Monday",
-            1: "Tuesday",
-            2: "Wednesday",
-            3: "Thursday",
-            4: "Friday"
+            0: "Sunday",
+            1: "Monday",
+            2: "Tuesday",
+            3: "Wednesday",
+            4: "Thursday",
+            5: "Friday",
+            6: "Saturday"
         }
 
         self.weeks = [[], [], [], [], []]
---
+++ Calendar/sheets.py
@@ -0,0 +1,6 @@
+# testing sheet reading here
+# if pip installation is having trouble: https://stackoverflow.com/questions/23708898/pip-is-not-recognized-as-an-internal-or-external-command
+# pip install gspread oauth2client
+
+import gspread
+from oauth2client.serice_account import ServiceAccountCredentials
