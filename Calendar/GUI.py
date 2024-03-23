99f61c873e56fce122e6b4276ee4fb3e99e49800
danielfmonzon <monzond@ufl.edu>
2024-03-23 12:52:37 -0400

    New GUI features

--- Calendar/GUI.py
+++ Calendar/GUI.py
@@ -14,31 +14,36 @@ class CalendarApp:
         self.title_label = tk.Label(self.main_frame, text = "CourseCal", font = ('Times New Roman', 20))
         # Pack the label at the top of the window
         self.title_label.pack(side = tk.TOP)
+        
         self.draw_calendar()
         
     def draw_calendar(self):
         # Create a frame for the days of the week labels
         days_frame = tk.Frame(self.main_frame)
-        days_frame.pack(fill=tk.X, expand=True)
+        days_frame.pack(fill = tk.X, expand = True)
         days_of_the_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
     
         # Create and pack labels for the days of the week
         for day in days_of_the_week:
-            day_label = tk.Label(days_frame, text=day)
-            day_label.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+            day_label = tk.Label(days_frame, text = day)
+            day_label.pack(side = tk.LEFT, expand = True, fill = tk.BOTH)
 
+        day_number = 1 # starting value
         # Create the calendar with weeks and days
         for week in range(4):
             # Create a frame for each week
             week_frame = tk.Frame(self.main_frame)
-            week_frame.pack(fill=tk.X, expand=True)
+            week_frame.pack(fill = tk.X, expand = True)
 
-            for day in range(5):
-                day_button = tk.Button(week_frame, text=f'{day + 1 + week * 5}',
-                                    command=lambda d=day: self.day_selected(d))
+            for _ in range(5): # 5 days per week
+                day_button = tk.Button(week_frame, text = str(day_number),
+                                       command = lambda d = day_number: self.day_selection(d))
                 # Pack day buttons side by side within the week frame, ensuring they fill out the row
-                day_button.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
+                day_button.pack(side = tk.LEFT, fill = tk.BOTH, expand = True)
+                day_number += 1
+                
+            day_number += 2 # Skip ahead by 2 days after each week
+            
     def day_selected(self, day):
         # Placeholder for what happens when a day is selected, such as opening a new window to add/view events
         #messagebox.showinfo("Day Selected", f"Day {day + 1} selected.")
@@ -47,7 +52,6 @@ class CalendarApp:
 def main():
     root = tk.Tk()
     app = CalendarApp(root)
-    #app.draw_calendar()
     root.geometry("600x400")  # You can adjust the size as needed
     root.mainloop()
 
