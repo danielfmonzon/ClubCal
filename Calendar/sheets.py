f7b5a6d29ad328f73ba96812bbdcda1f0cb75f5d
Caleb Oxborough <oxboroughcaleb@gmail.com>
2024-03-23 16:10:09 -0400

    SHEET READING FUNCTIONALITY!

--- Calendar/sheets.py
+++ Calendar/sheets.py
@@ -3,4 +3,18 @@
 # pip install gspread oauth2client
 
 import gspread
-from oauth2client.serice_account import ServiceAccountCredentials
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("CourseCal_dataset").sheet1
+
+data = sheet.get_all_records()
+testcol = sheet.col_values(3)
+
+print(testcol)
+# print(data)
